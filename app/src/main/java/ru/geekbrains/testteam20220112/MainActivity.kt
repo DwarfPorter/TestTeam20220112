7c7971fff01769f763716d5f1cbf0f2ae6489c0e
DwarfPorter <mrvlmor@gmail.com>
2022-01-12 21:54:46 +0300

    изменил в коде приветствие

--- app/src/main/java/ru/geekbrains/testteam20220112/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/testteam20220112/MainActivity.kt
@@ -2,10 +2,14 @@ package ru.geekbrains.testteam20220112
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text = findViewById<TextView>(R.id.textview)
+        text.text = "Hello Team of dreams!"
     }
 }
